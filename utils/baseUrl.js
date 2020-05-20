2e400ac94b89c0e7f3c9796c5f80ec8c2e4de3e0
michalrutz <michal.rutz@gmail.com>
2020-05-20 14:37:27 +0200

    baseUrl update the link

--- utils/baseUrl.js
+++ utils/baseUrl.js
@@ -1,6 +1,6 @@
 const baseUrl =
   process.env.NODE_ENV === "production"
-    ? "https://react-reserve.now.sh"
+    ? 'https://next-tes.herokuapp.com/'
     : "http://localhost:3000";
 
 export default baseUrl;
